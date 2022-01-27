68fb06bfec2788ea2db667bab4b68b419986750a
Nachomegalfa <nachopbe@gmail.com>
2022-01-27 16:56:10 +0100

    drogaController

--- src/main/java/com/HFA/app/controlador/DrogaController.java
+++ src/main/java/com/HFA/app/controlador/DrogaController.java
@@ -4,10 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.HFA.app.entidad.Droga;
 import com.HFA.app.repositorio.DrogaRepository;
@@ -21,12 +29,64 @@ public class DrogaController {
 
 	@GetMapping
 	public List<Droga> devolverDrogas(){
-		return repositorio.findAll();
+		return this.repositorio.findAll();
 	}
 	
 	@GetMapping("/id/{id}")
 	public Optional<Droga> devolverUnaDrogaId(@PathVariable("id") Integer id){
-		return repositorio.findById(id);
+		return this.repositorio.findById(id);
+	}
+	
+	@PostMapping
+	@ResponseBody
+	public String anyadirDroga(@RequestBody Droga droga) {
+		this.repositorio.save(droga);
+		
+		return "Perfecto ya se ha guardado la droga que me has mandado!!!";
+	}
+
+	@DeleteMapping("/id/{id}")
+	@ResponseBody
+	public ResponseEntity<?> borrarDroga (@PathVariable("id") Integer idDroga){
+		
+		Optional<Droga> drogaActual = this.repositorio.findById(idDroga);
+		
+		if (!drogaActual.isPresent()) {
+			System.out.println("No existe la droga con el id: " + idDroga);
+			
+			return new ResponseEntity<>("Error, no existe la droga con id: " + idDroga, HttpStatus.NOT_FOUND);
+		}
+		
+		this.repositorio.deleteById(idDroga);
+		
+		return new ResponseEntity<String>("Se ha borrado!", HttpStatus.OK);
+		
+	}
+	
+	@PutMapping("/id/{id}")
+	@ResponseBody
+	public ResponseEntity<?> modificarDroga(@PathVariable("id") Integer idDroga, @RequestBody Droga nuevaDroga){
+		
+		System.out.println("Vamos a modificar la droga con id + " + idDroga);
+		Optional<Droga> drogaActual = this.repositorio.findById(idDroga);
+		
+		if (!drogaActual.isPresent()) {
+		
+			System.out.println("No existe la droga con id " + idDroga);
+			return new ResponseEntity("Error, no existe el cliente con id " + idDroga, HttpStatus.NOT_FOUND);
+		}
+		
+		drogaActual.get().setNombre(nuevaDroga.getNombre());
+		drogaActual.get().setLegalidad(nuevaDroga.getLegalidad());
+		drogaActual.get().setPsicoactivo(nuevaDroga.getPsicoactivo());
+		drogaActual.get().setPrecioGramo(nuevaDroga.getPrecioGramo());
+		drogaActual.get().setProveedor(nuevaDroga.getProveedor());
+		drogaActual.get().setPedidos(nuevaDroga.getPedidos());
+		
+		this.repositorio.save(drogaActual.get());
+		return new ResponseEntity<Droga>(drogaActual.get(), HttpStatus.OK);
+		
 	}
 	
 }
+
--- src/main/java/com/HFA/app/repositorio/DrogaRepository.java
+++ src/main/java/com/HFA/app/repositorio/DrogaRepository.java
@@ -1,9 +1,11 @@
 package com.HFA.app.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.HFA.app.entidad.Droga;
 
+@Repository
 public interface DrogaRepository extends JpaRepository<Droga, Integer>{
 
 }
