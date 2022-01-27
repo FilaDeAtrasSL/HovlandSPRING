deb1db227e5965f2693c502df6fe2cc4ea79fe4a
Nachomegalfa <nachopbe@gmail.com>
2022-01-27 16:35:16 +0100

    drogaController

--- src/main/java/com/HFA/app/controlador/DrogaController.java
+++ src/main/java/com/HFA/app/controlador/DrogaController.java
@@ -1,5 +1,32 @@
 package com.HFA.app.controlador;
 
-public class DrogaController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.HFA.app.entidad.Droga;
+import com.HFA.app.repositorio.DrogaRepository;
+
+@RestController
+@RequestMapping("drogas")
+public class DrogaController {
+	
+	@Autowired
+	DrogaRepository repositorio;
+
+	@GetMapping
+	public List<Droga> devolverDrogas(){
+		return repositorio.findAll();
+	}
+	
+	@GetMapping("/id/{id}")
+	public Optional<Droga> devolverUnaDrogaId(@PathVariable("id") Integer id){
+		return repositorio.findById(id);
+	}
+	
 }
