012c13820cab319f84ab193b7ecd7d753d31f424
nachopbe@gmail.com <nachopbe@gmail.com>
2022-02-06 13:54:00 +0100

    terminado

---
+++ src/main/java/com/HFA/app/controlador/HelloController.java
@@ -0,0 +1,13 @@
+package com.HFA.app.controlador;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+	
+	
+	
+	
+}
